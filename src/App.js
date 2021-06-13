ce9141177b65a882493e53b48c889b95868f68e3
samlv99 <samlv3112@gmail.com>
2021-06-13 17:10:05 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import logo from "./logo.svg";
 import "./App.css";
 import { Switch } from "antd";
@@ -7,6 +6,7 @@ function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <h2>Hello Sam</h2>
     </div>
   );
 }
